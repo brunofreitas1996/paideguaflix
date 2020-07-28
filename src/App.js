c9d5932d27d53eab6c68f7babeafbaa4a38925a2
Bruno Freitas <51858121+brunofreitas1996@users.noreply.github.com>
2020-07-28 20:58:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,12 +29,6 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[3]}      
       />
-      <Carousel
-        category={dadosIniciais.categorias[4]}      
-      />
-      <Carousel
-        category={dadosIniciais.categorias[5]}      
-      />
       <Footer />
       
     </div>
