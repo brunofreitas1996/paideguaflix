c611d1766a887e8c48bbff72f883290db834a5ea
Bruno Freitas <freitasbruno965@gmail.com>
2020-07-28 22:30:08 -0300

    Commit rapidao aqui

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu';
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import Menu from '../../components/Menu';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
 
-function App() {
+function Home() {
   return (
     <div style={{background: "#141414"}}>
       
@@ -13,7 +13,7 @@ function App() {
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end?"}
+        videoDescription={"Pará!"}
       />
       <Carousel
         ignoreFirstVideo
@@ -29,15 +29,10 @@ function App() {
       <Carousel
         category={dadosIniciais.categorias[3]}      
       />
-      <Carousel
-        category={dadosIniciais.categorias[4]}      
-      />
-      <Carousel
-        category={dadosIniciais.categorias[5]}      
-      />
       
+      <Footer/>
     </div>
   );
 }
 
-export default App;
+export default Home;
